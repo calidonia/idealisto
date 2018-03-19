5f4ca80c0347ce87aff180db74020c85ca09633d
Carlos Neira Cortizas <calidonia.hibernia@gmail.com>
2018-03-19 21:01:32 +0100

    Update idealisto.R

--- R/idealisto.R
+++ R/idealisto.R
@@ -288,7 +288,7 @@ idealisto <- function(url, area, ruta = "~/idealisto.csv") {
     if (length(cocinha) == 0) {cocinha <- NA}
     if (length(amoblado) == 0) {amoblado <- NA}
 
-    if (length(str_extract(pattern = "Certificaci\u00F3n energ\uEE09tica: (\{\d+:\d+\}\,\{\d+:\d+\} kWh/m\u00B2 a\u00F1o$",
+    if (length(str_extract(pattern = "Certificaci\u00F3n energ\uEE09tica: ([[:digit:]]+\\,*[[:digit:]]* kWh/m\u00B2 a\u00F1o$",
                            string = detalles)) > 0) {
       cert_enerx <-1
       kwh_m2_ano <- as.numeric(str_replace_all(pattern = "\,",
