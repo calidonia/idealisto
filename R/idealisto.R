4897048e5690b5ed3bbf746302a97ba10f217e6b
Carlos Neira Cortizas <calidonia.hibernia@gmail.com>
2018-03-21 00:02:37 +0100

    Update idealisto.R

--- R/idealisto.R
+++ R/idealisto.R
@@ -404,10 +404,10 @@ idealisto <- function(url, area, ruta = "~/idealisto.csv") {
     
     process <- 100 - ((p/length(links_anuncios_tot))*100)
     
-    print(paste0(links_anuncios_tot[p], "\n")
-    print(paste0(line, "\n")
-    print(paste0("Idealisto leva descargados o ", round(process, digits = 1),"% dos anuncios.\n"))
-    print("-------------------------------------------------\n")
+    cat(paste0(links_anuncios_tot[p], "\n"))
+    cat(paste0(line, "\n")
+    cat(paste0("Idealisto leva descargados o ", round(process, digits = 1),"% dos anuncios.\n"))
+    cat("-------------------------------------------------\n")
 
     write.table(line, file = ruta, sep = ";", append = TRUE, quote = TRUE, col.names = FALSE, row.names = FALSE, na = "")
 
